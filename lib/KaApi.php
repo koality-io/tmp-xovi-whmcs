94092fd0c4056823cc0a4e8a7f10fd9eb30b749c
Lauris Binde <lauris@lauris.lv>
2023-01-19 09:25:27 +0100

    Add KA tracking parameter

--- lib/KaApi.php
+++ lib/KaApi.php
@@ -66,7 +66,7 @@ final class KaApi
         ];
 
         try {
-            $response = $this->client->post('/jsonrest/business-partner/30/keys?return-key-state=yes', $options);
+            $response = $this->client->post('/jsonrest/business-partner/30/keys?return-key-state=yes&retailer=whmcs-ka', $options);
             $data = json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
         } catch (\Throwable $e) {
             throw new Exception($e->getMessage(), 0, $e);
